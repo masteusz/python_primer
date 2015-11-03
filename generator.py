4e06e060ded95c6be0cc4c408695d241461b8471
Mateusz Stankiewicz <mstankiewicz@egnyte.com>
2015-11-03 10:31:32 +0100

    Simple generator, renamed grep.

--- generator.py
+++ generator.py
@@ -1,14 +1,8 @@
-def grep(pattern):
-    print("Looking for", pattern)
-    while True:
-        # What happens here?
-        line = (yield)
-        if pattern in line:
-            print(line)
+def countdown(cnt=10):
+    for i in range(cnt, -1, -1):
+        yield i
+
 
 if __name__ == "__main__":
-    g = grep("test")
-    next(g)
-
-    g.send("What is the new version of python?")
-    g.send("I am testing this now")
+    for i in countdown(15):
+        print(i)
---
+++ grep_using_generator.py
@@ -0,0 +1,14 @@
+def grep(pattern):
+    print("Looking for", pattern)
+    while True:
+        # What happens here?
+        line = (yield)
+        if pattern in line:
+            print(line)
+
+if __name__ == "__main__":
+    g = grep("test")
+    next(g)
+
+    g.send("What is the new version of python?")
+    g.send("I am testing this now")
